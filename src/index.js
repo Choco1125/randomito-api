33dcb82b96153de6b428e527b79a4b801b442ba1
Choco1125 <sebascruz981125@gmail.com>
2022-01-26 00:02:06 -0500

    Init

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const morgan = require("morgan");
+const randomFloat = require("./useCases/randomFloat");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  morgan(
+    `:method :url port=${PORT} :status :res[content-length] - :response-time ms`
+  )
+);
+
+app.get("/api/v1/number", (req, res) => {
+  const value = randomFloat.randomFloat();
+  res.json({ value });
+});
+
+app.listen(PORT, () => {
+  console.log(`Randomito API runing on port ${PORT}`);
+});
---
+++ src/test/randomFloat.test.js
@@ -0,0 +1,13 @@
+const ramdonFloat = require("./../useCases/randomFloat");
+
+describe("RandomFloat", () => {
+  it("shoul return a random float", () => {
+    const generateRadomFloat = ramdonFloat.randomFloat();
+
+    expect(typeof generateRadomFloat).toBe("number");
+    expect(generateRadomFloat <= 1).toEqual(true);
+    expect(generateRadomFloat >= 0).toEqual(true);
+
+    expect(generateRadomFloat).not.toBe(null);
+  });
+});
